2b2ae2279f1e7fdd488d7f44e8e57c899df2e560
KingDede <jason-campbell23@hotmail.com>
2016-04-21 08:08:44 +0100

    pls fix

--- app/src/main/java/jason/networkapp/RemoteDBHelper.java
+++ app/src/main/java/jason/networkapp/RemoteDBHelper.java
@@ -51,22 +51,22 @@ public class RemoteDBHelper {
 
         @Override
         protected ListObject doInBackground(Void... params) {
-            BufferedReader rd  = null;
-            StringBuilder response = null;
-            String line = null;
+            BufferedReader rd;
+            StringBuilder sb = null;
+            String line;
             try {
                 URL url = new URL(rootURL + "getalldata");
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("GET");
                 connection.connect();
                 rd  = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                response = new StringBuilder();
+                sb = new StringBuilder();
 
                 while ((line = rd.readLine()) != null)
                 {
-                    response.append(line + '\n');
+                    sb.append(line + '\n');
                 }
-                Log.e("Response: ", response.toString());
+                Log.e("Response: ", sb.toString());
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -75,14 +75,17 @@ public class RemoteDBHelper {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            String response = sb.toString();
 
             JSONObject jsonResult = null;
-            try {
-                jsonResult = new JSONObject(response.toString());
-                Log.d("JSON Object: ", jsonResult.toString());
-                Log.d("JSON Object Length: ","" + jsonResult.length());
-            } catch (JSONException e) {
-                e.printStackTrace();
+            if (response != null) {
+                try {
+                    jsonResult = new JSONObject(response);
+                    Log.d("JSON Object: ", jsonResult.toString());
+                    Log.d("JSON Object Length: ", "" + jsonResult.length());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
 
             ArrayList<Spec> spec = new ArrayList<>();
@@ -90,107 +93,110 @@ public class RemoteDBHelper {
             ArrayList<Trait> trait = new ArrayList<>();
             ArrayList<SkillProf> skillProf = new ArrayList<>();
 
-            JSONArray specializations;
-            try {
-                specializations = jsonResult.getJSONArray("Specializations");
-                for (int i = 0; i < specializations.length(); i++) {
-                    JSONObject jsonArrayObject = specializations.getJSONObject(i);
-                    int spec_id = Integer.parseInt((String) jsonArrayObject.get("spec_id"));
-                    String name = jsonArrayObject.get("name").toString();
-                    int elite = Integer.parseInt((String) jsonArrayObject.get("elite"));
-                    String icon = jsonArrayObject.get("icon").toString();
-                    String background = jsonArrayObject.get("background").toString();
-                    String profession = jsonArrayObject.get("profession").toString();
-                    // Log for debugging.
-                    Log.e("spec object:", "" + spec_id + ", " + name + ", " + elite + ", " + icon + ", " + background + ", " + profession);
-                    spec.add(new Spec(spec_id, name, elite, icon, background, profession));
+            if (jsonResult != null) {
+                JSONArray specializations;
+                try {
+                    specializations = jsonResult.getJSONArray("Specializations");
+                    for (int i = 0; i < specializations.length(); i++) {
+                        JSONObject jsonArrayObject = specializations.getJSONObject(i);
+                        int spec_id = Integer.parseInt((String) jsonArrayObject.get("spec_id"));
+                        String name = jsonArrayObject.get("name").toString();
+                        int elite = Integer.parseInt((String) jsonArrayObject.get("elite"));
+                        String icon = jsonArrayObject.get("icon").toString();
+                        String background = jsonArrayObject.get("background").toString();
+                        String profession = jsonArrayObject.get("profession").toString();
+                        // Log for debugging.
+                        Log.e("spec object:", "" + spec_id + ", " + name + ", " + elite + ", " + icon + ", " + background + ", " + profession);
+                        spec.add(new Spec(spec_id, name, elite, icon, background, profession));
+                    }
+                } catch (JSONException e) {
+                    // Log exceptions
+                    e.printStackTrace();
                 }
-            } catch (JSONException e) {
-                // Log exceptions
-                e.printStackTrace();
-            }
 
-            JSONArray skills;
-            try {
-                skills = jsonResult.getJSONArray("Skills");
-                for (int i = 0; i < skills.length(); i++) {
-                    JSONObject jsonArrayObject = skills.getJSONObject(i);
-                    int skill_id = Integer.parseInt((String) jsonArrayObject.get("skill_id"));
-                    String name = jsonArrayObject.get("name").toString();;
-                    String description = jsonArrayObject.get("description").toString();
-                    String type = jsonArrayObject.get("type").toString();
-                    String weapon_type = jsonArrayObject.get("weapon_type").toString();
-                    String slot = jsonArrayObject.get("slot").toString();;
-                    String category = jsonArrayObject.get("category").toString();
-                    String attunement = jsonArrayObject.get("attunement").toString();
-                    String toolbelt_skill = jsonArrayObject.get("toolbelt_skill").toString();
-                    int cost = Integer.parseInt((String) jsonArrayObject.get("cost"));
-                    String dual_wield = jsonArrayObject.get("dual_wield").toString();
-                    int flip_skill = Integer.parseInt((String) jsonArrayObject.get("flip_skill"));
-                    int initiative = Integer.parseInt((String) jsonArrayObject.get("initiative"));
-                    int next_chain = Integer.parseInt((String) jsonArrayObject.get("next_chain"));
-                    int prev_chain = Integer.parseInt((String) jsonArrayObject.get("prev_chain"));
-                    // Log for debugging.
-                    Log.e("skill object:", "" + skill_id + ", " + name + ", " + description + ", " + type + ", " + weapon_type + ", " + slot + ", " + category + ", " + attunement +
-                            ", " + toolbelt_skill + ", " + cost + ", " + dual_wield + ", " + flip_skill + ", " + initiative + ", " + next_chain + ", " + prev_chain);
-                    skill.add(new Skill(skill_id, name, description, type, weapon_type, slot, category, attunement, toolbelt_skill, cost, dual_wield, flip_skill, initiative, next_chain, prev_chain));
+                JSONArray skills;
+                try {
+                    skills = jsonResult.getJSONArray("Skills");
+                    for (int i = 0; i < skills.length(); i++) {
+                        JSONObject jsonArrayObject = skills.getJSONObject(i);
+                        int skill_id = Integer.parseInt((String) jsonArrayObject.get("skill_id"));
+                        String name = jsonArrayObject.get("name").toString();
+                        String description = jsonArrayObject.get("description").toString();
+                        String type = jsonArrayObject.get("type").toString();
+                        String weapon_type = jsonArrayObject.get("weapon_type").toString();
+                        String slot = jsonArrayObject.get("slot").toString();
+                        String category = jsonArrayObject.get("category").toString();
+                        String attunement = jsonArrayObject.get("attunement").toString();
+                        String toolbelt_skill = jsonArrayObject.get("toolbelt_skill").toString();
+                        int cost = Integer.parseInt((String) jsonArrayObject.get("cost"));
+                        String dual_wield = jsonArrayObject.get("dual_wield").toString();
+                        int flip_skill = Integer.parseInt((String) jsonArrayObject.get("flip_skill"));
+                        int initiative = Integer.parseInt((String) jsonArrayObject.get("initiative"));
+                        int next_chain = Integer.parseInt((String) jsonArrayObject.get("next_chain"));
+                        int prev_chain = Integer.parseInt((String) jsonArrayObject.get("prev_chain"));
+                        // Log for debugging.
+                        Log.e("skill object:", "" + skill_id + ", " + name + ", " + description + ", " + type + ", " + weapon_type + ", " + slot + ", " + category + ", " + attunement +
+                                ", " + toolbelt_skill + ", " + cost + ", " + dual_wield + ", " + flip_skill + ", " + initiative + ", " + next_chain + ", " + prev_chain);
+                        skill.add(new Skill(skill_id, name, description, type, weapon_type, slot, category, attunement, toolbelt_skill, cost, dual_wield, flip_skill, initiative, next_chain, prev_chain));
+                    }
+                } catch (JSONException e) {
+                    // Log exceptions
+                    e.printStackTrace();
                 }
-            } catch (JSONException e) {
-                // Log exceptions
-                e.printStackTrace();
-            }
 
-            JSONArray traits;
-            try {
-                traits = jsonResult.getJSONArray("Traits");
-                for (int i = 0; i < traits.length(); i++) {
-                    JSONObject jsonArrayObject = traits.getJSONObject(i);
-                    int trait_id = Integer.parseInt((String) jsonArrayObject.get("trait_id"));
-                    String name = jsonArrayObject.get("name").toString();
-                    String description = jsonArrayObject.get("description").toString();
-                    int spec_id = Integer.parseInt((String) jsonArrayObject.get("spec_id"));
-                    int tier = Integer.parseInt((String) jsonArrayObject.get("tier"));
-                    String slot = jsonArrayObject.get("slot").toString();
-                    String icon = jsonArrayObject.get("icon").toString();
-                    // Log for debugging.
-                    Log.e("trait object:", "" + trait_id + ", " + name + ", " + description + ", " + spec_id + ", " + tier + ", " + slot+ ", " + icon);
-                    trait.add(new Trait(trait_id, name, description, spec_id, tier, slot, icon));
+                JSONArray traits;
+                try {
+                    traits = jsonResult.getJSONArray("Traits");
+                    for (int i = 0; i < traits.length(); i++) {
+                        JSONObject jsonArrayObject = traits.getJSONObject(i);
+                        int trait_id = Integer.parseInt((String) jsonArrayObject.get("trait_id"));
+                        String name = jsonArrayObject.get("name").toString();
+                        String description = jsonArrayObject.get("description").toString();
+                        int spec_id = Integer.parseInt((String) jsonArrayObject.get("spec_id"));
+                        int tier = Integer.parseInt((String) jsonArrayObject.get("tier"));
+                        String slot = jsonArrayObject.get("slot").toString();
+                        String icon = jsonArrayObject.get("icon").toString();
+                        // Log for debugging.
+                        Log.e("trait object:", "" + trait_id + ", " + name + ", " + description + ", " + spec_id + ", " + tier + ", " + slot + ", " + icon);
+                        trait.add(new Trait(trait_id, name, description, spec_id, tier, slot, icon));
+                    }
+                } catch (JSONException e) {
+                    // Log exceptions
+                    e.printStackTrace();
                 }
-            } catch (JSONException e) {
-                // Log exceptions
-                e.printStackTrace();
-            }
 
-            JSONArray skillprof;
-            try {
-                skillprof = jsonResult.getJSONArray("Skill_Prof");
-                for (int i = 0; i < skillprof.length(); i++) {
-                    JSONObject jsonArrayObject = skillprof.getJSONObject(i);
-                    int skill_id = Integer.parseInt((String) jsonArrayObject.get("skill_id"));
-                    int elementalist = Integer.parseInt((String) jsonArrayObject.get("elementalist"));
-                    int mesmer = Integer.parseInt((String) jsonArrayObject.get("mesmer"));
-                    int necromancer = Integer.parseInt((String) jsonArrayObject.get("necromancer"));
-                    int ranger = Integer.parseInt((String) jsonArrayObject.get("ranger"));
-                    int thief = Integer.parseInt((String) jsonArrayObject.get("thief"));
-                    int engineer = Integer.parseInt((String) jsonArrayObject.get("engineer"));
-                    int warrior = Integer.parseInt((String) jsonArrayObject.get("warrior"));
-                    int guardian = Integer.parseInt((String) jsonArrayObject.get("guardian"));
-                    int revenant = Integer.parseInt((String) jsonArrayObject.get("revenant"));
+                JSONArray skillprof;
+                try {
+                    skillprof = jsonResult.getJSONArray("Skill_Prof");
+                    for (int i = 0; i < skillprof.length(); i++) {
+                        JSONObject jsonArrayObject = skillprof.getJSONObject(i);
+                        int skill_id = Integer.parseInt((String) jsonArrayObject.get("skill_id"));
+                        int elementalist = Integer.parseInt((String) jsonArrayObject.get("elementalist"));
+                        int mesmer = Integer.parseInt((String) jsonArrayObject.get("mesmer"));
+                        int necromancer = Integer.parseInt((String) jsonArrayObject.get("necromancer"));
+                        int ranger = Integer.parseInt((String) jsonArrayObject.get("ranger"));
+                        int thief = Integer.parseInt((String) jsonArrayObject.get("thief"));
+                        int engineer = Integer.parseInt((String) jsonArrayObject.get("engineer"));
+                        int warrior = Integer.parseInt((String) jsonArrayObject.get("warrior"));
+                        int guardian = Integer.parseInt((String) jsonArrayObject.get("guardian"));
+                        int revenant = Integer.parseInt((String) jsonArrayObject.get("revenant"));
 
-                    // Log for debugging.
-                    Log.e("skill_prof object:", "" + skill_id + ", " + elementalist + ", " + mesmer + ", " + necromancer + ", " + ranger + ", " + thief + ", " + engineer +
-                            ", " + warrior + ", " + guardian + ", " + revenant);
-                    skillProf.add(new SkillProf(skill_id, elementalist, mesmer, necromancer, ranger, thief, engineer, warrior, guardian, revenant));
+                        // Log for debugging.
+                        Log.e("skill_prof object:", "" + skill_id + ", " + elementalist + ", " + mesmer + ", " + necromancer + ", " + ranger + ", " + thief + ", " + engineer +
+                                ", " + warrior + ", " + guardian + ", " + revenant);
+                        skillProf.add(new SkillProf(skill_id, elementalist, mesmer, necromancer, ranger, thief, engineer, warrior, guardian, revenant));
+                    }
+                } catch (JSONException e) {
+                    // Log exceptions
+                    e.printStackTrace();
                 }
-            } catch (JSONException e) {
-                // Log exceptions
-                e.printStackTrace();
             }
             ListObject output = new ListObject(spec, skill, trait, skillProf);
             Log.e("do in background result", "" + output.spec_list.get(0).name);
             return output;
         }
 
+        //its broke
         @Override
         protected void onPostExecute(ListObject result) {
             allOutput = result;
